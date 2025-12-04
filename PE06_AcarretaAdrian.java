3516c77a7644bf1d226e4d4d10d15685bb6db505
Adrian Acarreta <alu.adrian.acarreta@mataro.epiaedu.cat>
2025-12-04 19:47:01 +0100

    Métodos newGame, chooseNames, chooseNumPlayers y setDefaultValues

--- PE06_AcarretaAdrian.java
+++ PE06_AcarretaAdrian.java
@@ -17,10 +17,12 @@ public class PE06_AcarretaAdrian {
     public void principal() {
         Scanner s = new Scanner(System.in);
         int n=chooseNumPlayers(s);
-        System.out.println(n);
-        int[] players = new int[n];
+        String[] players = new String[n];
         int[] positions = new int[n];
         int[] penalties = new int[n];
+        setDefaultValues(positions, penalties);
+        chooseNames(s, players);
+        newGame(players, positions, penalties);
     }
 
     public int chooseNumPlayers(Scanner s) {
@@ -38,6 +40,50 @@ public class PE06_AcarretaAdrian {
         return number;
     }
 
+    public void setDefaultValues(int[] positions, int[] penalties) {
+        for (int i=0;i<positions.length;i++) {
+            positions[i]=0;
+            penalties[i]=0;
+        }
+    }
+
+    public void newGame(String[] players, int[] positions, int[] penalties) {
+        int dices=2;
+        int turns=0;
+        Boolean end=false;
+
+        while (!end) {
+            for (int i=0;i<players.length;i++) {
+
+                turns++;
+            }
+        }
+    }
+
+    public void newTurn(String[] players, int[] positions, int[] penalties, int turns, int dices) {
+        int dice1=throwDices();
+
+        if (turns==0) {
+
+        }
+    }
+
+    public int throwDices() {
+        int number=(int)(Math.random()*6)+1;
+        return number;
+    }
+
+    //public void chooseOrder(int[] players,int n) {
+    //    (int)(Math.random()*n)+1;
+    //}
+
+    public void chooseNames(Scanner s, String[] players) {
+        for (int i=0;i<players.length;i++) {
+            System.out.printf(YELLOW+"\n(?) Please enter player %s name: ",(i+1)+RESET);
+            players[i] = s.nextLine();
+        }
+    }
+
     public int readInt(Scanner s) {
         int number=0;
         Boolean validNum=false;
@@ -47,12 +93,12 @@ public class PE06_AcarretaAdrian {
                 validNum=true;
             } catch (InputMismatchException e) {
                 System.out.println(RED+"(!) Please enter a valid integer number."+RESET);
-                s.nextLine();
                 continue;
             } catch (Exception e) {
                 System.out.println(RED+"(!) Unknown error: "+e+RESET);
-                s.nextLine();
                 continue;
+            } finally {
+                s.nextLine();
             }
         }
         return number;

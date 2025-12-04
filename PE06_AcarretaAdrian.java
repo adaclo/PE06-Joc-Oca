0eda18ed27b51b790465a2ab5d8975f983f1a4ef
Adrian Acarreta <alu.adrian.acarreta@mataro.epiaedu.cat>
2025-12-04 16:08:55 +0100

    Creació de fitxers

---
+++ PE06_AcarretaAdrian.java
@@ -0,0 +1,11 @@
+package Activitats.PE06;
+
+public class PE06_AcarretaAdrian {
+    public static void main(String[] args) {
+        PE06_AcarretaAdrian p = new PE06_AcarretaAdrian();
+        p.principal();
+    }
+    public void principal() {
+        
+    }
+}
